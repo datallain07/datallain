222966285cd910be4a43ba9a9dbc96a9a07c3ef2
datff1900@gmail.com <Datall07#>
2025-07-24 09:20:55 +0700

    update skins.js

--- aov/skins.js
+++ aov/skins.js
@@ -1,3 +1,13 @@
 const skins = [
-
+{
+champion: "Nakroth",
+name: "Xiao",
+IOS: "https://link4m.com/ihY9gGLg",
+Android: "https://link4m.com/QcuSACv",
+bgImg: "img/xiao.png",
+miniImg: "img/xiao_mini.png",
+label: "Mới",
+desc: "Effects - Sound - Button",
+color: "0, 255, 200",
+}
 ];
