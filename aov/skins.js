9e6ad7c84f1d5a5b80cc36098fa4d2edfe4e8419
datff1900@gmail.com <Datall07#>
2025-07-24 09:47:22 +0700

    update skins.js

--- aov/skins.js
+++ aov/skins.js
@@ -1,5 +1,16 @@
 const skins = [
 {
+champion: "Thorne",
+name: "Thứ Nguyên Vệ Thần",
+IOS: "https://link4m.com/O4cM5N6C",
+Android: "https://link4m.com/SCxrDdt",
+bgImg: "img/13314.jpg",
+miniImg: "img/13314head.jpg",
+label: "Mới",
+desc: "Effects - Sound - Button",
+color: "50, 150, 255",
+},
+{
 champion: "Nakroth",
 name: "Xiao",
 IOS: "https://link4m.com/ihY9gGLg",
@@ -10,5 +21,4 @@ label: "Mới",
 desc: "Effects - Sound - Button",
 color: "0, 255, 200",
 }
-
 ];
