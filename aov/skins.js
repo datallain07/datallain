deac4eabf36674bfcadad85008d93adf60c0d61e
datff1900@gmail.com <Datall07#>
2025-07-24 09:54:48 +0700

    update skins.js

--- aov/skins.js
+++ aov/skins.js
@@ -2,8 +2,8 @@ const skins = [
 {
 champion: "Thorne",
 name: "Thứ Nguyên Vệ Thần",
-IOS: "https://link4m.com/O4cM5N6C",
-Android: "https://link4m.com/SCxrDdt",
+IOS: "https://link4m.com/hCS9pdPc",
+Android: "https://link4m.com/j8RC873",
 bgImg: "img/13314.jpg",
 miniImg: "img/13314head.jpg",
 label: "Mới",
