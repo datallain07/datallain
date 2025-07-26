1291c4fdea8b195579114f7b54b79b4748a49e4c
datff1900@gmail.com <Datall07#>
2025-07-26 11:12:52 +0700

    update

--- aov/skins.js
+++ aov/skins.js
@@ -1,5 +1,16 @@
 const skins = [
 {
+champion: "Wiro",
+name: "Mặc định",
+IOS: "https://link4m.com/HP8hrEE",
+Android: "https://link4m.com/HP8hrEE",
+bgImg: "img/19400.jpg",
+miniImg: "img/301940head.jpg",
+label: "Mới",
+desc: "Effects - Sound - Button",
+color: "255, 200, 50",
+},
+{
 champion: "Tel'Annas",
 name: "Thứ Nguyên Vệ Thần",
 IOS: "https://link4m.com/Ip1ydAGZ",
